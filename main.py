76e08a94ed19e6696c491998d93731225c2a1954
ibrahimqaws <mribrahemalrawi1997@gmail.com>
2025-12-19 18:39:11 +0300

    Initial commit - version 1

---
+++ main.py
@@ -0,0 +1,8 @@
+from pet import Pet
+
+# إنشاء كائن (Object) من فئة Pet
+my_pet = Pet("Buddy", 80)
+
+# طباعة الاسم ومستوى الطاقة
+print(f"Pet Name: {my_pet.name}")
+print(f"Energy Level: {my_pet.energy_level}")
---
+++ pet.py
@@ -0,0 +1,4 @@
+class Pet:
+    def init(self, name, energy_level):
+        self.name = name
+        self.energy_level = energy_level
